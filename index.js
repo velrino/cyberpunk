3936ad89632ac9b69d3eac6dbe2273100fad1c59
velrino <denismagalhaesvelrino@gmail.com>
2018-07-20 19:10:59 -0300

    Structure of project

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+const express = require('express');
+const routes = require("./src/infrastructure/routes");
+const typeorm = require("typeorm");
+const Helpers = require("./src/infrastructure/helpers");
+
+typeorm.createConnection({
+  type: "postgres",
+  host: "127.0.0.1",
+  port: 5432,
+  username: "postgres",
+  password: "root",
+  database: "cart",
+  synchronize: true,
+  logging: false,
+  entities: Helpers.entities()
+}).then(async connection => {
+
+  var bodyParser = require('body-parser')
+
+  var app = express();
+  app.use(bodyParser.json());
+
+  routes.forEach(route => {
+    app[route.method](route.path, (request, response, next) => {
+      route.action(request, response)
+        .then(() => next)
+        .catch(err => next(err));
+    });
+  });
+
+  app.listen(3000, function () {
+    console.log('Port 3000!');
+  });
+
+}).catch(error => console.log("TypeORM connection error: ", error));
