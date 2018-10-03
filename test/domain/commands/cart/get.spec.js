b1568841ddb29fc507ffe84033daf31de2359f45
velrino <denismagalhaesvelrino@gmail.com>
2018-10-03 11:57:35 -0300

    Refacotry TDD because SRC change

--- test/domain/commands/cart/get.spec.js
+++ test/domain/commands/cart/get.spec.js
@@ -8,16 +8,19 @@ import mockCart from '../../../fixtures/cart';
 const getCartCommand = new GetCartCommand({ cartRepository });
 const getCartCommandNotFound = new GetCartCommand({ cartRepository: cartRepositoryNotFound });
 
+const { cartUid } = mockCart;
+const params = { cartUid };
+
 describe('GetCartCommand', () => {
   describe('Execute()', () => {
     test('Return Cart object', (done) => {
-      const result = getCartCommand.execute();
+      const result = getCartCommand.execute({ params });
       expect(result.body).toMatchObject(mockCart);
       expect(result.status).toBe(statusCode.OK);
       done();
     });
     test('Return message when cart not found with status 404', (done) => {
-      const result = getCartCommandNotFound.execute();
+      const result = getCartCommandNotFound.execute({ params });
       expect(result.body).toMatchObject(ENUMS.message.cart.notFound);
       expect(result.status).toBe(statusCode.NOT_FOUND);
       done();
