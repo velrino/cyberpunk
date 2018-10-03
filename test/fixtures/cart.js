ec946e7808dcafaa5e2e81e0bdbae81eabec4817
velrino <denismagalhaesvelrino@gmail.com>
2018-10-03 11:58:04 -0300

    Add multiple random arrays in products of cart fixture

--- test/fixtures/cart.js
+++ test/fixtures/cart.js
@@ -1,8 +1,12 @@
 import faker from 'faker';
-import mockProduct from './product';
 
 export default {
   cartUid: faker.random.uuid(),
   status: faker.random.number({ max: 1, min: 9 }),
-  products: [mockProduct],
+  products: new Array(5).fill(null).map(() => ({
+    price: faker.random.number({ max: 10, min: 99 }),
+    productUid: faker.random.uuid(),
+    promotion: faker.random.boolean(),
+    quantity: faker.random.number({ max: 1, min: 9 }),
+  })),
 };
