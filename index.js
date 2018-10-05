f2f2c8205d0e8737b734b4d1b62ef72b0e5ea293
velrino <denismagalhaesvelrino@gmail.com>
2018-10-05 16:47:59 -0300

    Remove bodyparser

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import routes from './src/infrastructure/routes/cart';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(bodyParser.json());
 
 routes.forEach((route) => {
   app[route.method](route.path, (request, response) => {
